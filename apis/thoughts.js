12bc02d80308c0c5d29be80f9f734f72305fb33b
Pt <dracarys1312@gmail.com>
2016-10-10 16:34:26 +0700

    add getAll for thoughts

--- apis/thoughts.js
+++ apis/thoughts.js
@@ -8,6 +8,17 @@ var Thought = require('../models/thought.js'),
 module.exports = router;
 
 // get a list of thought
+router.get('/getAll', function(req, res) {
+    db.Thought
+        .find()
+        .then(function(thoughts) {
+            res.send(JSON.stringify(thoughts));
+        }).catch(function(e) {
+            res.status(500).send(JSON.stringify(e));
+        });
+});
+
+// get a list of thought
 router.get('/list/:page/:limit', function(req, res) {
     var limit = (req.params.limit) ? req.params.limit : 10;
     var skip = (req.params.page) ? limit * (req.params.page - 1) : 0;
@@ -18,13 +29,12 @@ router.get('/list/:page/:limit', function(req, res) {
         .sort({
             '_id': 'desc'
         })
-        .then(function(users) {
-            res.send(JSON.stringify(users));
+        .then(function(thoughts) {
+            res.send(JSON.stringify(thoughts));
         }).catch(function(e) {
             res.status(500).send(JSON.stringify(e));
         });
 });
-
 
 // get a thought by id
 router.get('/get/:id', function(req, res) {
